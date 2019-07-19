3d83cdaa242d3671404b24180ac38ca2350ba6fe
shidongyang <851051279@qq.com>
2019-07-19 20:08:07 +0800

    整理格式

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import Header from '@/components/Header/Header'
-import Main from '@/components/Main/Main'
+import React, { Component } from 'react';
+import Header from '@/components/Header/Header';
+import Main from '@/components/Main/Main';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Main />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Header />
+				<Main />
+			</div>
+		);
+	}
 }
