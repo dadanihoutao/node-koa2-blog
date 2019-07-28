b247edaed9d34515d8613e1c9c90df2a6d194637
shidongyang <851051279@qq.com>
2019-07-28 18:28:57 +0800

    调整数据库

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
 module.exports = {
     dbName: 'yangblog',
-    dbPass: 'shi19910213',
-    dbHost: 'localhost',
-    dbUser: 'root',
+    // dbPass: 'shi19910213',
+    // dbHost: 'localhost',
+    // dbUser: 'root',
+    dbPass: 'Test123456!',
+    dbHost: '127.0.0.1',
+    dbUser: 'test',
     dbPort: 3306,
     port: 3001,
 
