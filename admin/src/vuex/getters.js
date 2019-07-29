5a10949a941b762ea0ef55cac3f1f1843bd4865e
shidongyang <851051279@qq.com>
2019-07-29 23:38:41 +0800

    简单的加了一下测菜单栏

--- admin/src/vuex/getters.js
+++
@@ -1,7 +0,0 @@
-/**
- * @Author: 时洋
- * @Last Modified by: shiyang
- * @Last Modified time: 2019-05-13 14:05:37
- */
-export default {
-}
--- admin/src/vuex/mutations.js
+++
@@ -1,7 +0,0 @@
-/**
- * @Author: 时洋
- * @Last Modified by: shiyang
- * @Last Modified time: 2019-05-15 16:36:15
- */
-export default {
-}
