75b8e87cccd1dd44530df79212d18c7db2399091
shidongyang <851051279@qq.com>
2020-02-23 15:14:30 +0800

    在来一次

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ const env = process.env.NODE_ENV;
 
 // 开发环境数据库配置
 const devConfig = {
-    dbName: '',
-    dbPass: '',
+    dbName: 'yangblog',
+    dbPass: 'shi19910213',
     dbHost: 'localhost',
     dbUser: 'root',
     dbPort: 3306,
@@ -15,13 +15,13 @@ const devConfig = {
 }
 // 线上环境数据库配置
 const protConfig = {
-    dbName: '',
-    dbPass: '',
+    dbName: 'yangblog',
+    dbPass: 'Test123456!',
     dbHost: '127.0.0.1',
     dbUser: 'test',
     dbPort: 3306,
     timezone: '08:00',
-    host: ''
+    host: 'http://admin.lokiblog.com'
 }
 
 let config = {}
